6d7bb59d05a9c3b414484c39cb08ed4e803dd37b
Lelouch <flashgrinder92@gmail.com>
2020-08-09 13:39:53 +0400

    Сверстал слайдер, поменял стили основных кнопок, исправил модификаторы классов

---
+++ app/js/scripts.min.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    const menu = ['F-TRIP', 'F-EDUCATION', 'F-CAREER', 'F-RULES']
+    const mySwiper = new Swiper ('.head-slider', {
+        // If we need pagination
+        slidesPerView: 1,
+        loop: true,
+        effect: 'fade',
+        pagination: {
+        el: '.head-slider__pagination',
+                clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="head-slider__bullet ' + className + '">' + (menu[index]) + '</span>';
+            },
+        }
+    })
+
+})
